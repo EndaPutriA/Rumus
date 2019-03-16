94a8da134f396d6d397b8ea3c25f7a07297b4c47
EndaPutriA <endaputri240899@gmail.com>
2019-03-16 06:47:21 -0700

    Inisialisasi Project

---
+++ luaslingkaran.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ float main()
+ {
+ 	float phi=3.14;
+ 	float r;
+ 	
+ 	printf("masukan nilai jari jari = ");
+ 	scanf("%f",&r);
+ 	
+ 	float luas_lingkaran=phi*r*2;
+ 	
+ 	printf("luas lingkaran adalah = %.2f",luas_lingkaran);
+ 	
+ }
